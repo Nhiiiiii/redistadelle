6795d254a64d767c516d49bdb0ed596cca4bba6c
ni <thomas.sensebe@gmail.com>
2013-09-07 15:42:37 +0200

    Add login page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,14 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Http.Context;
 import play.mvc.Result;
+import play.data.*;
 import views.html.admin;
+import views.html.index;
 import views.html.redistadelle;
+
+import static play.data.Form.*;
 
 /**
  * Application pages controller.
@@ -12,8 +17,30 @@ import views.html.redistadelle;
  */
 public class Application extends Controller {
   
+	public static class Login {
+        public String playerId;
+    }
+	
+	/** Index/Login page */
+	public static Result index() {
+		if(Context.current().session().get("playerId").isEmpty()) {
+			return ok(index.render(form(Login.class)));
+		}
+		return redirect(routes.Application.redistadelle());
+	}
+	
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if(!loginForm.get().playerId.isEmpty()) {
+        	 session("playerId", loginForm.get().playerId);
+        	 return redirect(routes.Application.redistadelle());
+        }
+        return badRequest(index.render(loginForm));
+        
+    }
+	
 	/** Game page */
-    public static Result index() {
+    public static Result redistadelle() {
         return ok(redistadelle.render());
     }
     
